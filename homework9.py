bf96ff5b1405baec9816f2559f96ad50fca4e45e
alfa1991 <72934972+alfa1991@users.noreply.github.com>
2024-04-23 00:34:10 +0500

    Add files via upload

---
+++ homework9.py
@@ -0,0 +1,9 @@
+def print_params(param):
+    # Функция для печати переданного значения дважды.
+    print(param)
+    print(param)
+
+# Вызов функции несколько раз
+print_params("Hello")
+print_params(123)
+print_params([1, 2, 3])
